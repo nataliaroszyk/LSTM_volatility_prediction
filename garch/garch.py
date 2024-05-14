a87d9f38752984f84062850e0c6448ad86bca177
Natalia Roszyk <109170120+nataliaroszyk@users.noreply.github.com>
2024-05-14 14:51:11 +0400

    Update garch.py

--- garch/garch.py
+++ garch/garch.py
@@ -1,42 +1,11 @@
 import pandas as pd
 import numpy as np
 from arch import arch_model
+from garch.garch_functions import garch_data_prep, train_garch_model
 
 # Download the dataset
-sp = pd.read_excel('data/sp500.xlsx')
-sp['Date'] = pd.to_datetime(sp['Date'])  # Ensure 'Date' is in datetime format
-
-# Calculate daily log returns
-sp['log_returns'] = np.log(sp['Close'] / sp['Close'].shift(1))
-
-# Calculate rolling volatility using log returns
-rolling_window_size = 22
-sp['volatility'] = sp['log_returns'].rolling(window=rolling_window_size).std()
-
-# Scale log returns for model input
-sp['scaled_log_returns'] = sp['log_returns'] * 100
-sp.dropna(inplace=True)
-
-# Filter data starting from 1985 to use the first 15 years for training
-sp = sp[sp.Date >= '1985-01-01']
-
-def train_garch_model(data, start_date):
-    """ Train GARCH model and predict future volatility based on historical log returns. """
-    train_data = data[data['Date'] < pd.to_datetime(start_date)]
-    train_size = len(train_data)
-
-    rolling_predictions = []
-    for i in range(train_size, len(data)):
-        train = data['scaled_log_returns'][i - train_size:i]
-        model = arch_model(train, vol='Garch', p=2, q=2)
-        model_fit = model.fit(disp='off')
-        pred = model_fit.forecast(horizon=1, reindex=False)
-        rolling_predictions.append(np.sqrt(pred.variance.values[-1, :][0]))
-
-    predictions_df = pd.DataFrame(rolling_predictions, index=data['Date'][train_size:])
-    predictions_df.columns = ['prediction']
-    predictions_df['prediction'] = predictions_df['prediction'] / 100
-    return predictions_df
+df = pd.read_excel('data/sp500.xlsx')
+sp = garch_data_prep(df)
 
 predicted_volatility = train_garch_model(sp, '2000-01-03')
 
